b3009b83777674c410556a36d9a55782928dccb7
Richard Kalehoff <richardkalehoff@gmail.com>
2015-11-19 12:18:26 -0500

    set up click handler for menu items

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -3,4 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'li',
   classNames: ['menu-item'],
+  actions: {
+    chooseItem(menuCategory, menuItemName) {
+      console.log(menuCategory, menuItemName);
+    }
+  }
 });
