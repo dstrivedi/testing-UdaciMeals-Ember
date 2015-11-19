e85a2d40441527b1ddce2d7101d0cbda8530ddb0
Richard Kalehoff <richardkalehoff@gmail.com>
2015-11-19 12:02:35 -0500

    convert static order-tracker HTML to dynamic template

--- app/services/order-manager.js
+++ app/services/order-manager.js
@@ -1,5 +1,47 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  selectedDay: 'Monday'
+  selectedDay: 'Monday',
+  menuSelection: {
+    Monday: {
+      salad: '',
+      sandwich: '',
+      meal: '',
+      dessert: '',
+      drink: '',
+      snack: ''
+    },
+    Tuesday: {
+      salad: '',
+      sandwich: '',
+      meal: '',
+      dessert: '',
+      drink: '',
+      snack: ''
+    },
+    Wednesday: {
+      salad: '',
+      sandwich: '',
+      meal: '',
+      dessert: '',
+      drink: '',
+      snack: ''
+    },
+    Thursday: {
+      salad: '',
+      sandwich: '',
+      meal: '',
+      dessert: '',
+      drink: '',
+      snack: ''
+    },
+    Friday: {
+      salad: '',
+      sandwich: '',
+      meal: '',
+      dessert: '',
+      drink: '',
+      snack: ''
+    }
+  }
 });
